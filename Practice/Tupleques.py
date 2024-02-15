95eaeedfa99e04dbc7eb811687ef90f66c7258a4
Vikram Jha <vikramjha17373@gmail.com>
2024-02-16 01:11:36 +0530

    new file:   Practice/Tupleques.py

---
+++ Practice/Tupleques.py
@@ -0,0 +1,11 @@
+# WAP to count the number of students with "A" grade in following tuple.
+
+grade = ("C", "D", "A", "A", "B", "B", "A")
+print(grade.count("A"))
+
+
+# Store the above value in a list and sort them from "A" to "D".
+
+grade_list = list(grade)
+grade_list.sort()
+print(grade_list)
