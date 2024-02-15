dec6fef4e53b2fd00722ddd296a214453eae1349
Vikram Jha <vikramjha17373@gmail.com>
2024-02-16 00:53:28 +0530

    new file:   Practice/Listques.py

---
+++ Practice/Listques.py
@@ -0,0 +1,17 @@
+#WAP to ask the user to enter names of their 3 favourite movies and store them in a list.
+
+
+Movies = []
+movie1 = input("Enter the name of First movie : ")
+movie2 = input("Enter the name of Second movie : ")
+movie3 = input("Enter the name of Third movie : ")
+
+
+Movies.append(movie1)
+Movies.append(movie2)
+Movies.append(movie3)
+
+print(Movies)
+
+
+# WAP to check if a list contains a palindrome of elements.
