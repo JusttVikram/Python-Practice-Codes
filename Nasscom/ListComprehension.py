0394a303212ebf1c7b4343f70720070dbb3f3d2a
Vikram Jha <vikramjha17373@gmail.com>
2024-02-20 09:55:33 +0530

    modified:   Nasscom/ListComprehension.py

--- Nasscom/ListComprehension.py
+++ Nasscom/ListComprehension.py
@@ -2,12 +2,10 @@ numbers = [1, 2, 3, 4, 5]
 
 # List comprehension to create a new list with squared values
 squared_numbers = [num ** 2 for num in numbers]
-
-# Print the squared numbers
 print(squared_numbers)
 
 
 #List Comprehension for filteration
 
-s = [i for i in range(10) if i%2==0]
-print(s)
+Even_numbers = [i for i in range(10) if i%2==0]
+print(Even_numbers)
