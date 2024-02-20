f678553247596681218d0f4d598bd027e97e8518
Vikram Jha <vikramjha17373@gmail.com>
2024-02-20 16:11:04 +0530

    new file:   Practice/Anagrams.py 	new file:   Practice/Permutations of String.py

---
+++ Practice/Permutations of String.py
@@ -0,0 +1,24 @@
+def permute(string):
+    if len(string) == 0:
+        return []
+
+    if len(string) == 1:
+        return [string]
+
+    permutations = []
+
+    for i in range(len(string)):
+        
+        current_char = string[i]
+
+        remaining_chars = string[:i] + string[i+1:]
+        for perm in permute(remaining_chars):
+            
+            permutations.append(current_char + perm)
+
+    return permutations
+
+
+input_string = input("Enter a string: ")
+result = permute(input_string)
+print("All permutations of the string:", result)
