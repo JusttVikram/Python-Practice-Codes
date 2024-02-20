107d167d69f796224adad546ddb88233203a1d1c
Vikram Jha <vikramjha17373@gmail.com>
2024-02-20 14:28:38 +0530

    new file:   Nasscom/List Comprehension.py 	deleted:    Nasscom/ListComprehension.py

---
+++ Nasscom/List Comprehension.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5]
+
+# List comprehension to create a new list with squared values
+squared_numbers = [num ** 2 for num in numbers]
+print(squared_numbers)
+
+
+#List Comprehension for filteration
+
+Even_numbers = [i for i in range(10) if i%2==0]
+print(Even_numbers)
+
+# Walrus Operator
+if (name := input("Enter your name: ")) == "Vikram":
+    print("Hello, Vikram!")
+else:
+    print("Hello, stranger!")
