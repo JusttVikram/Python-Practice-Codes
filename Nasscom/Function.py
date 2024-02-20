38fe1078159961828246ac4d96a16b0865d44016
Vikram Jha <vikramjha17373@gmail.com>
2024-02-20 10:14:42 +0530

    new file:   Nasscom/Function.py 	modified:   Nasscom/ListComprehension.py 	new file:   Nasscom/Modules.py

---
+++ Nasscom/Function.py
@@ -0,0 +1,18 @@
+def calculator(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operator"
+
+
+result = calculator(5, 3, '+')
+print(result)
---
+++ Nasscom/Modules.py
@@ -0,0 +1,8 @@
+# Calender
+
+import calendar
+print(calendar.month(2024,3))
+
+# Keywords
+import keyword
+print(keyword.kwlist)
