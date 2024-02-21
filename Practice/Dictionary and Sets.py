19093499c01b6ff13a76517d55507fb2bcb758e9
Vikram Jha <vikramjha17373@gmail.com>
2024-02-21 22:14:06 +0530

    modified:   Practice/Dictionary and Sets.py

--- Practice/Dictionary and Sets.py
+++ Practice/Dictionary and Sets.py
@@ -6,5 +6,30 @@ info = {
 
     }
 
-print(info["course name"])
-print(info["Duration"])
+# Nested dictionary
+
+student = {
+    "name" : "Vikram",
+    "Subjects" : {
+        "Physics" : 99,
+        "Chemistry" : 99,
+        "Maths" : 100
+    }
+}
+
+
+print(student["Subjects"])
+print(student["Subjects"]["Physics"])
+print(student["Subjects"]["Chemistry"])
+print(student["Subjects"]["Maths"])
+print(student["name"])
+
+print(student.keys())
+print(student.values())
+print(student.items())
+
+
+print(student.get("name"))
+student.update({"City" : "Delhi"})
+print(student["City"])
+print(student)
