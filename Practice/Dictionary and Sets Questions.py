68af569a2ff4000f0100677764f1269023fffc2c
Vikram Jha <vikramjha17373@gmail.com>
2024-02-21 23:46:12 +0530

    modified:   Practice/Dictionary and Sets Questions.py

--- Practice/Dictionary and Sets Questions.py
+++ Practice/Dictionary and Sets Questions.py
@@ -1,18 +1,18 @@
 
 # Store word meanings in python dictionary
 
-# word_meanings = {'table' : ['a piece of furniture','list of facts and figures'] , 'cats' : 'a small animal'}
-# print(word_meanings.keys())
-# print(word_meanings['table'])
-# print(word_meanings['cats'])
+word_meanings = {'table' : ['a piece of furniture','list of facts and figures'] , 'cats' : 'a small animal'}
+print(word_meanings.keys())
+print(word_meanings['table'])
+print(word_meanings['cats'])
 
 
 
 # You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students.
 
-# subjects = {'Python','java','C++','Python','javascript','java','Python','java','C++','C'} 
+subjects = {'Python','java','C++','Python','javascript','java','Python','java','C++','C'} 
 
-# print(f'Number of classrooms required is {len(subjects)}.')
+print(f'Number of classrooms required is {len(subjects)}.')
 
 
 # WAP to enter marks of 3 subjects from the user and store them in dictionary. Start with an empty dictionary and add one by one. Use subject name as key and marks as value.
@@ -29,3 +29,8 @@ chemistry = int(input('Enter the marks of Chemistry : '))
 marks.update({'Chemistry' : chemistry})
 
 print(marks)
+
+
+# Figure out a way to store 9 & 9.0 as seperate values in the set.
+values = {9, "9.0"}
+print(values)
