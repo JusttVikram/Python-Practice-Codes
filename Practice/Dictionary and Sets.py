c28ef7138b963d6518f17ed02be30900a65e8200
Vikram Jha <vikramjha17373@gmail.com>
2024-02-21 19:53:25 +0530

    new file:   Practice/Dictionary and Sets.py

---
+++ Practice/Dictionary and Sets.py
@@ -0,0 +1,10 @@
+info = {
+    "course name" : "Python",
+    "Topics" : ("List and Tuples", "Dictionary and Sets"),
+    "Duration" : 120,
+    "is_mandatory" : True
+
+    }
+
+print(info["course name"])
+print(info["Duration"])
