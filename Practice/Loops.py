14b73dc250256416ea3e3b49799ca8d6df61a6c8
vikram jha <vikramjha17373@gmail.com>
2024-02-23 23:15:50 +0530

    modified:   Practice/Loops.py

--- Practice/Loops.py
+++ Practice/Loops.py
@@ -1,8 +1,13 @@
-dicti = {'name': 'Vikram', 'age': 23}
+squares = (1,4,9,16,25,36,49,64,81,100)
 
-print(dicti)
-print(dicti['name'], dicti['age'])
-print(dicti.keys())
-print(dicti.values())
+# x = int(input("Enter a number in between 1 and 10 : "))
+x = 49
 
+i = 0
 
+while i < len(squares):
+    if squares[i] == x :
+        print(True, i)
+    else:
+        print(False)
+    i+=1
