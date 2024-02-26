d3ceddd736d0fed5185a0dc1f953fa548674df66
Vikram Jha <Vikramjha17373@gmail.com>
2024-02-26 13:22:49 +0530

    modified:   Nasscom/OOPs/Inheritance.py

--- Nasscom/OOPs/Inheritance.py
+++ Nasscom/OOPs/Inheritance.py
@@ -19,12 +19,48 @@ class Cat(Animal):
     def speak(self):
         print(f"{self.name} meows.")
 
-# Creating instances of the classes
-animal = Animal("Generic Animal")
-dog = Dog("Buddy")
-cat = Cat("Whiskers")
+class A:
+    def __init__(self):
+        self.num1 = int(input("Enter the number 1 : "))
+        self.num2 = int(input("Enter the number 2 : "))
 
-# Calling the speak method
-animal.speak()
-dog.speak()
-cat.speak()
+    def Add(self):
+        return self.num1 + self.num2
+    def Subtract(self):
+        return self.num1 - self.num2
+    def multiply(self):
+        return self.num1* self.num2
+    def division(self):
+        return self.num1/ self.num2
+    def remainder(self):
+        return self.num1 % self.num2
+    
+
+class B:
+    back = "Oracle Database and java"
+    def backend(self):
+        print("Back task implementation ", self.back)
+
+class C:
+    front = "HTML, CSS, JS"
+    def frontend(self):
+        print("Frontend task implementation", self.front)
+
+class D (B,C):
+    def show(self):
+        print("Dynamic web created.")
+
+object = D()
+object.backend()
+object.frontend()
+object.show()
+
+# Creating instances of the classes
+# animal = Animal("Generic Animal")
+# dog = Dog("Buddy")
+# cat = Cat("Whiskers")
+
+# # Calling the speak method
+# animal.speak()
+# dog.speak()
+# cat.speak()
