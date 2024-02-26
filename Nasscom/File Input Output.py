d9b7f500a7d514d8805d6c46538d2283c0a2f27d
vikram jha <vikramjha17373@gmail.com>
2024-02-27 01:25:29 +0530

    modified:   Nasscom/File Input Output.py 	modified:   Nasscom/Sample.txt

--- Nasscom/File Input Output.py
+++ Nasscom/File Input Output.py
@@ -1,7 +1,15 @@
-f = open("Sample.txt", "r")
-data = f.read()      #reads entire file.
-# data1 = f.readline()  #reads one line at a time.
-print(data)
-print(type(data))
+"""Readind a file."""
 
+# f = open("Sample.txt", "r")
+# data = f.read()      #reads entire file.
+# # data1 = f.readline()  #reads one line at a time.
+# print(data)
+# print(type(data))
+# f.close()
 
+"""Writing a file."""
+
+# f = open("Sample.txt", "w")  #Overrites the entire file.
+f = open("Sample.txt", "a")    #adds to the file.
+f.write("Yeh hai naya content.\n")
+f.close()
