88e57acb3be3eed045ba562e895dd7f5d11223b7
vikram jha <vikramjha17373@gmail.com>
2024-02-27 23:35:39 +0530

    deleted:    Nasscom/File Input Output.py 	new file:   Nasscom/File Input Output/Reading File.py 	new file:   Nasscom/File Input Output/Sample.txt 	new file:   Nasscom/File Input Output/Writing File.py 	new file:   Nasscom/File Input Output/demo.txt 	deleted:    Nasscom/Sample.txt

---
+++ Nasscom/File Input Output/Writing File.py
@@ -0,0 +1,7 @@
+# f = open("Sample.txt", "w")  #Overrites the entire file.
+# f = open("Sample.txt", "a")    #adds to the file.
+# f.write("Yeh hai naya content.\n")
+# f.close()
+
+f = open("demo.txt", "w")
+f.close()
