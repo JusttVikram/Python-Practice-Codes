6005ff5f9eccb47dfb5c591f99d5b700b9f90ab1
vikram jha <vikramjha17373@gmail.com>
2024-03-01 09:25:29 +0530

    new file:   Nasscom/OOPs/Apna College.py

---
+++ Nasscom/OOPs/Apna College.py
@@ -0,0 +1,15 @@
+class student:
+    def __init__(self):
+        print("Adding new student to the database...")
+    name = input('Enter the name of student : ')
+
+s1 = student()
+print("Student's name is : ",s1.name)
+
+# class car:
+#     color = "Black"
+#     brand = "Toyota"
+
+# c1 = car()
+# print(c1.brand)
+# print(c1.color)
