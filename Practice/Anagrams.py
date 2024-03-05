e4b78583fa999bda6bebfdb4f7efda63beaa0355
vikram jha <vikramjha17373@gmail.com>
2024-03-05 10:54:14 +0530

    modified:   Practice/Permutations of String.py 	new file:   Practice/remove vowels.py

--- Practice/Anagrams.py
+++ Practice/Anagrams.py
@@ -1,9 +1,9 @@
 def is_anagram(str1, str2):
-    
     str1 = str1.replace(" ", "").lower()
     str2 = str2.replace(" ", "").lower()
 
     return sorted(str1) == sorted(str2)
 
-print(is_anagram("test","nest"))
-print(is_anagram("test","tset"))
+
+print(is_anagram("test", "nest"))
+print(is_anagram("test", "tset"))
