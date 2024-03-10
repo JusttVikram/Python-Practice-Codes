d38bcc3c23a4fbfb904a8b5d4c26deee15491b38
Vikram Jha <vikramjha17373@gmail.com>
2024-03-10 17:07:45 +0530

    new file:   Django/calc/__init__.py 	new file:   Django/calc/admin.py 	new file:   Django/calc/apps.py 	new file:   Django/calc/migrations/__init__.py 	new file:   Django/calc/models.py 	new file:   Django/calc/tests.py 	new file:   Django/calc/views.py

--- Practice/Occurence Of Element.py
+++ Practice/Occurence Of Element.py
@@ -1,13 +1,27 @@
-def count(list, x):
+def Count(list, target):
     count = 0
     for i in list:
-        if i == x:
+        if i == target:
             count = count + 1
 
     return count
 
 
-list = [1, 1, 1, 2, 2, 4, 5, 6, 8, 65, 0]
-x = 34
+def char_count_for_loop(String):
+    char_dict = {}
+    for char in string:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
 
-print(f"{x} has {count(list, x)} occurrences.")
+
+string = "abracadabra"
+char_frequency = char_count_for_loop(string)
+print(char_frequency)
+
+list1 = [1, 1, 1, 2, 2, 4, 5, 6, 8, 65, 0]
+target = 34
+
+# print(f"{x} has {Count(list, x)} occurrences.")
