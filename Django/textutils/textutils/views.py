96f51c4421aa9aee0f91f639f864ab8d1bbd6760
Vikram Jha <vikramjha17373@gmail.com>
2024-03-22 01:56:51 +0530

    modified:   Django/textutils/templates/index.html 	new file:   Django/textutils/templates/index2.html 	modified:   Django/textutils/textutils/views.py

--- Django/textutils/textutils/views.py
+++ Django/textutils/textutils/views.py
@@ -12,16 +12,20 @@ def index(request):
 def analyze(request):
     # Get the text input from the request
     input_text = request.GET.get('text','default')
-    
     # Check if the removepunc parameter is set to 'on'
     removepunc = request.GET.get('removepunc','off')
     # Check if the uppercase parameter is set to 'on'
     uppercase = request.GET.get('uppercase','off')
+    # Check if the newlineremover parameter is set to 'on'
+    newlineremover = request.GET.get('newlineremover','off')
+    # Check if the spaceremover parameter is set to 'on'
+    extraspaceremover = request.GET.get('extraspaceremover','off')
+    # Check if the charcounter parameter is set to 'on'
+    charcounter = request.GET.get('charcounter','off')
     
     if removepunc == 'on':
 
         analyzed = ""
-
         punctuations = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
 
         for char in input_text:
@@ -29,7 +33,7 @@ def analyze(request):
                 analyzed = analyzed + char
 
         # Create a dictionary of parameters to pass to the analyze.html template
-        params = {'purpose':'Remove Punctuations', 'analyzed_text': analyzed}
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
         
         # Render the analyze.html template with the parameters
         return render(request, 'analyze.html', params)
@@ -38,9 +42,31 @@ def analyze(request):
         analyzed = ''
         for char in input_text:
             analyzed = analyzed + char.upper()
-        params = {'purpose':'Change to UpperCase', 'analyzed_text': analyzed}
+
+        params = {'purpose':'Changed to UpperCase', 'analyzed_text': analyzed}
         return render(request, 'analyze.html', params)
     
+    elif(newlineremover=='on'):
+        analyzed = ''
+        for char in input_text:
+            if char !='\n':
+                analyzed = analyzed + char
+
+        params = {'purpose':'Removed new lines.', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    
+    elif(extraspaceremover=='on'):
+        analyzed = ' '.join(input_text.split())
+        
+        params = {'purpose':'Removed spaces.', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    
+    elif(charcounter=='on'):
+
+        analyzed = (f'{input_text} has {len(input_text)} characters.')
+
+        params = {'purpose':'Counted Characters.', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
 
     else:
         return HttpResponse('Error. Please tick the checkbox.')
