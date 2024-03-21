e466291a2090128b042299f7066f2fe014ec6420
Vikram Jha <vikramjha17373@gmail.com>
2024-03-22 00:55:18 +0530

    modified:   Django/textutils/textutils/__pycache__/views.cpython-312.pyc 	modified:   Django/textutils/textutils/views.py

--- Django/textutils/textutils/views.py
+++ Django/textutils/textutils/views.py
@@ -1,19 +1,42 @@
 # Self created file.
+
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    # return HttpResponse("Home")
+    # Render the index.html template
     return render(request, 'index.html')
+
+
 def analyze(request):
+    # Get the text input from the request
     text = request.GET.get('text','default')
+    
+    # Check if the removepunc parameter is set to 'on'
+    removepunc = request.GET.get('removepunc','off')
+    
+    if removepunc == 'on':
+        # Initialize an empty string to store the analyzed text
+        analyzed = ""
+        
+        # Define a string of punctuation characters
+        punctuations = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
 
-    analyzed = ""
-    punctuations = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
+        # Iterate over each character in the input text
+        for char in text:
+            # Check if the character is not a punctuation character
+            if char not in punctuations:
+                # Append the character to the analyzed string
+                analyzed = analyzed + char
 
-    for char in text:
-        if char not in punctuations:
-            analyzed = analyzed + char
+        # Create a dictionary of parameters to pass to the analyze.html template
+        params = {'purpose':'Remove Punctuations', 'analyzed_text': analyzed}
+        
+        # Render the analyze.html template with the parameters
+        return render(request, 'analyze.html', params)
+    
+    else:
+        # If the removepunc parameter is not set to 'on', return an error message
+        return HttpResponse('Error. Please check the box.')
 
-    params = {'purpose':'Remove Punctuations', 'analyzed_text': analyzed}
-    return render(request, 'analyze.html', params)
