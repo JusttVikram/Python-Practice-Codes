f018c28c33951f9595f52b52d4b1f30bee6483d6
Vikram <vikramjha17373@gmail.com>
2024-07-09 14:20:20 -0400

    modified:   Practice/K-largest_element.py

--- Practice/K-largest_element.py
+++ Practice/K-largest_element.py
@@ -7,10 +7,9 @@ def find_k_largest(arr, k):
     for num in arr[k:]:
         if num > heap[0]:
             heapq.heapreplace(heap, num)
-
     return heap
 
-arr = [5, 2, 1, 3, 7, 15, 25, 45, 18, 9, 12, 50, 3, 102, 30, 55, 88, 228]
+arr = [3, 102, 30, 55, 88, 228]
 k = 3
 result = find_k_largest(arr, k)
 print(f"The {k} largest elements in the array are: {result}")
