4c77d295fe2d9a9e614005f3f11fe7fb9084a78c
Vikram Jha <vikramjha17373@gmail.com>
2024-08-13 14:59:21 -0400

    modified:   Practice/Maximum Number of Vowels in a Substring of Given Length.py

--- Practice/Maximum Number of Vowels in a Substring of Given Length.py
+++ Practice/Maximum Number of Vowels in a Substring of Given Length.py
@@ -1,1 +1,18 @@
 # Leetcode 1456
+
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = set('aeiou')
+        n = len(s)
+        count = 0
+        for i in range(k):
+            if s[i] in vowels:
+                count += 1
+        max_count = count
+        for i in range(k, n):
+            if s[i] in vowels:
+                count += 1
+            if s[i - k] in vowels:
+                count -= 1
+            max_count = max(max_count, count)
+        return max_count
