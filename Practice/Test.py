fe3dd01fc48fa20a5ea81f81b8c7eb5a952ead98
Vikram Jha <vikramjha17373@gmail.com>
2024-08-15 15:18:25 -0400

    modified:   Practice/Test.py

--- Practice/Test.py
+++ Practice/Test.py
@@ -1,10 +1,12 @@
 # Given an array a of positive integers, find the number of pairs of integers for which the difference of the two numbers is equal to a number k. Since the number of pairs could be quite large, take modulo 10**9+7 for output.
 
 def solution(k, a):
-    a.sort()
+    freq = {}
     count = 0
-    for i in range(len(a)):
-        for j in range(i+1, len(a)):
-            if a[j] - a[i] == k:
-                count += 1
+    for num in a:
+        if num - k in freq:
+            count += freq[num - k]
+        if num + k in freq:
+            count += freq[num + k]
+        freq[num] = freq.get(num, 0) + 1
     return count % (10**9 + 7)
