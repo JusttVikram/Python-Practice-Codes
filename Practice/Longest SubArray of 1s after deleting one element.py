8588f3d43993d92bcdd44f1e21f64089a3586b93
Vikram Jha <vikramjha17373@gmail.com>
2024-08-15 16:25:50 -0400

    new file:   Practice/Longest SubArray of 1s after deleting one element.py

---
+++ Practice/Longest SubArray of 1s after deleting one element.py
@@ -0,0 +1,24 @@
+# Leetcode 1493
+
+from typing import List
+
+
+def longestSubarray(self, nums: List[int]) -> int:
+    left = 0
+    right = 0
+    max_length = 0
+    zero_count = 0
+
+    while right < len(nums):
+        if nums[right] == 0:
+            zero_count += 1
+
+        while zero_count > 1:
+            if nums[left] == 0:
+                zero_count -= 1
+            left += 1
+
+        max_length = max(max_length, right - left)
+        right += 1
+
+    return max_length
