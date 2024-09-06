0bb4e8510e96ee6839d74109bebd191db821f784
Vikram Jha <vikramjha17373@gmail.com>
2024-09-06 11:17:54 -0400

    modified:   Practice/Equal Row and Column Pairs.py

--- Practice/Equal Row and Column Pairs.py
+++ Practice/Equal Row and Column Pairs.py
@@ -4,16 +4,12 @@ from typing import List
 
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
+        out = 0
         n = len(grid)
-        row = [0]*n
-        col = [0]*n
         for i in range(n):
+            row = ",".join(map(str,grid[i]))
             for j in range(n):
-                row[i] += grid[i][j]
-                col[j] += grid[i][j]
-        ans = 0
-        for i in range(n):
-            for j in range(n):
-                if row[i] == col[j]:
-                    ans += 1
-        return ans
+                col = ','.join(str(grid[k][j]) for k in range(n))
+                if row == col:
+                    out += 1
+        return out
