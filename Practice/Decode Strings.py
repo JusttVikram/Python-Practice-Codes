66096ca1cbea93ba04886aa1b2355eb7bca7492d
Vikram Jha <vikramjha17373@gmail.com>
2024-09-13 13:01:02 -0400

    new file:   Practice/Decode Strings.py

---
+++ Practice/Decode Strings.py
@@ -0,0 +1,18 @@
+#  Leetcode 394
+
+
+def decodeString(s: str) -> str:
+    stack = []
+    for i in s:
+        if i != ']':
+            stack.append(i)
+        else:
+            temp = ''
+            while stack[-1] != '[':
+                temp = stack.pop() + temp
+            stack.pop()
+            num = ''
+            while stack and stack[-1].isdigit():
+                num = stack.pop() + num
+            stack.append(int(num) * temp)
+    return ''.join(stack)
