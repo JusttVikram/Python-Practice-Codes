0e003173fbd0241bb96f8a5ac525e35d2990151c
Vikram Jha <vikramjha17373@gmail.com>
2024-09-17 16:44:42 -0400

    modified:   Practice/Delete the middle node of a linked list.py

--- Practice/Delete the middle node of a linked list.py
+++ Practice/Delete the middle node of a linked list.py
@@ -1,5 +1,9 @@
 # Leetcode 2095
-
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         slow = head
