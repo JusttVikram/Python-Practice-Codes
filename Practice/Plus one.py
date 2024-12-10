79cd1faddc2578f0d905abfef4d9644b03ac628d
Vikram Jha <vikramjha17373@gmail.com>
2024-12-10 12:49:58 -0500

    new file:   Practice/Plus one.py

---
+++ Practice/Plus one.py
@@ -0,0 +1,14 @@
+# Leetcode 66
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        for i in range(len(digits)-1, -1, -1):
+            if carry == 0:
+                break
+            digits[i] += carry
+            carry = digits[i] // 10
+            digits[i] %= 10
+        if carry:
+            digits.insert(0, carry)
+        return digits
